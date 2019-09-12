c651561f99b19c0077d676279b18d9ce04c646e5
Marcos <m.benedicto84@gmail.com>
2019-09-12 16:36:32 -0400

    template change

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -6,7 +6,7 @@
     Haiku
   </div>
   <div class="hometext">
-    Haiku is a App that allows freelance poets to post their content. It is an app to share poetry among users.
+    Haiku is a App that allows freelance poets to post their content. It is an app to share poetry among users. Userc can take a look at the inspiration section to get random inspiration from other freelance poets around the web. Please post safely and create wonderful works of art.
 
   </div>
 
