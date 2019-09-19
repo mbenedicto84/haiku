766ebdb91237b8f9b3b78236f9e94c7e2377a339
Marcos <m.benedicto84@gmail.com>
2019-09-19 02:13:10 -0400

    fixed edit

--- models/post.model.js
+++ models/post.model.js
@@ -11,7 +11,7 @@ let Post = new Schema({
     type: String
   },
   date: {
-    type: Date
+    type: String
   },
   recommend: {
     type: String
--- src/components/IndexComponent.vue
+++ src/components/IndexComponent.vue
@@ -10,7 +10,7 @@
           <div v-for="post in posts" :key="post._id" class="grid">
             <h1> Title: <br/>{{ post.title }}</h1>
             <p> Comment: <br/>{{ post.body }}</p>
-                <p> Date: <br/>{{ moment()  }}</p>
+                <p> Date: <br/>{{ post.date }}</p>
             <p> Recommend: <br/>{{ post.recommend }}</p>
             <h3> by: <br/> {{ post.user}}</h3>
             <router-link :to="{name: 'edit', params: { id: post._id }}" class="btn btn-primary">Edit</router-link>
@@ -48,10 +48,9 @@ import moment from 'moment'
           this.posts.splice(this.posts.indexOf(id), 1);
         });
       },
-      moment () {
-     return moment(this.posts.date).format("MMMM D, YYYY");
-
-     }
+     //  moment () {
+     // return moment(this.post[0].date).slice(0, 5);
+     // }
 
 },
 
